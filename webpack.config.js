caeca36b49961a08910f5a67c71c760d63e0ec17
Wojciech Lukaszewicz <voytel@gmail.com>
2020-04-20 11:49:37 +1000

    P+w tweaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,28 @@
 const path = require('path')
-const webpack = require('webpack')
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+module.exports = {
 
-module.exports = (env) => {
-  const isProduction = env === 'production'
-  console.log('env', env)
-  console.log('isProdction', isProduction)
-
-  return {
-    entry: './src/app.js',
-    output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }, {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }]
-    },
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-      contentBase: path.join(__dirname, 'public')
-    }
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
   }
 }
