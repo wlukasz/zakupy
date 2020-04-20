24b66259b19f802a8781609dd89b50753902e3a9
Wojciech Lukaszewicz <voytel@gmail.com>
2020-04-20 14:14:14 +1000

    Resized checkbox

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,6 +4,7 @@ const Option = (props) => (
   <div className="option">
   <p className="option__text">{props.count}. {props.shopText}: {props.optionText}</p>
   <input 
+    className="chkbox"
     type="checkbox"
     checked={props.checked && "checked"} 
     onChange={(e) => {
