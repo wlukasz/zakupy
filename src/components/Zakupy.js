f3887a1c82b1199926122c172325ccc246692469
Wojciech Lukaszewicz <voytel@gmail.com>
2020-09-04 22:50:56 +1000

    Moved translations to config/ from Zakupy

--- src/components/Zakupy.js
+++ src/components/Zakupy.js
@@ -5,6 +5,7 @@ import React from 'react'
 import AddOption from './AddOption'
 import Header from './Header'
 import Options from './Options'
+import lingos from '../config/translations'
 
 export default class Zakupy extends React.Component {
   constructor(props) {
@@ -19,52 +20,6 @@ export default class Zakupy extends React.Component {
       undoQueue: [],
       selectedLingo: '',
       language: {},
-      lingos: [
-        {
-          lingo: 'en',
-          shopping: 'Shopping',
-          allItems: 'All Items',
-          thisShoppingTrip: 'Go Shopping!',
-          toggleTicks: 'All',
-          removeAll: 'Remove All',
-          allShops: 'All Shops',
-          deleteAllMsg1:
-            'Warning: OK will permanently remove all items. Push Cancel to avoid',
-          deleteAllMsg2:
-            'Note: To remove just one item Cancel this pop-up and swipe item to the left.',
-          removeItem: 'Remove',
-          addOptionMsg1: 'Enter valid value to add item',
-          addOptionMsg2: 'This item already exists',
-          emptyListMsg: 'Please add items to get started!',
-          itemPlaceholder: 'item',
-          shopPlaceholder: 'shop (optional)',
-          elsewhere: 'elsewhere',
-          addItemText: 'Add Item',
-          undoItem: 'Undo',
-        },
-        {
-          lingo: 'pl',
-          shopping: 'Zakupy',
-          allItems: 'Lista Ogólna',
-          thisShoppingTrip: 'Na Zakupy!',
-          toggleTicks: 'Wszystko',
-          removeAll: 'Wyczyść Listę',
-          allShops: 'Wszystkie Sklepy',
-          deleteAllMsg1:
-            'Uwaga: OK usunie wszystkie towary z listy. Cancel anuluje komendę.',
-          deleteAllMsg2:
-            'Notka: Aby usunąć jeden towar anuluj tę komendę i przesuń wybrany towar w lewo.',
-          removeItem: 'Usuń',
-          addOptionMsg1: 'Wpisz towar aby dodać go do listy',
-          addOptionMsg2: 'Ten towar został już wcześniej dodany do listy',
-          emptyListMsg: 'Aby zacząć dodaj towary do listy.',
-          itemPlaceholder: 'towar',
-          shopPlaceholder: 'sklep (opcjonalny)',
-          elsewhere: 'gdziekolwiek',
-          addItemText: 'Dodaj Towar',
-          undoItem: 'Wróć',
-        },
-      ],
     }
     this.filterShop = this.filterShop.bind(this)
     this.handleSetLingo = this.handleSetLingo.bind(this)
@@ -267,7 +222,6 @@ export default class Zakupy extends React.Component {
   }
 
   handleSetLingo(lingoToSet) {
-    const { lingos } = this.state
     this.setState(() => ({
       selectedLingo: lingoToSet,
       language: lingos.find(({ lingo }) => lingo === lingoToSet),
